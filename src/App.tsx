c61da5c0b9469ed6bfba5bccaf2d9cbf9beeca89
Keybeen <keybeen.videoeditor@gmail.com>
2026-02-27 22:27:44 +0800

    trying styling the component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { FieldInput } from "@/components/ui/Login"
 function App() {
   return (
     <>
-      <FieldInput />
+      <div className="flex justify-center border border-dashed border-gray-400 p-4">
+        <FieldInput/>
+      </div>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
       </div>
