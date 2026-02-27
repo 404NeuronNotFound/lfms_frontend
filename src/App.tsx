49b8ae99503e6cf44cf6f1768d9a1ff054d54bc8
Keybeen <keybeen.videoeditor@gmail.com>
2026-02-27 22:22:17 +0800

    still practicing shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button"
+import { FieldInput } from "@/components/ui/Login"
+
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <FieldInput />
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
   )
 }
 
