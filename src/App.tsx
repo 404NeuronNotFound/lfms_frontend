61610e62d366b3bd18755807bc4b095f72e15e0e
404NeuronNotFound <keybeen.videoeditor@gmail.com>
2026-02-28 23:24:44 +0800

    designing login components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { Button } from "@/components/ui/button"
+
 import { FieldInput } from "@/components/ui/Login"
 
 
 function App() {
   return (
     <>
-      <div className="flex justify-center border border-dashed border-gray-400 p-4">
         <FieldInput/>
-      </div>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
     </>
   )
 }
--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -6,26 +6,38 @@ import {
   FieldSet,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { Button } from "./button"
 
 export function FieldInput() {
   return (
-    <FieldSet className="w-full max-w-xs">
-      <FieldGroup>
-        <Field>
-          <FieldLabel htmlFor="username">Username</FieldLabel>
-          <Input id="username" type="text" placeholder="Max Leiter" />
-          <FieldDescription>
-            Choose a unique username for your account.
-          </FieldDescription>
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="password">Password</FieldLabel>
-          <FieldDescription>
-            Must be at least 8 characters long.
-          </FieldDescription>
-          <Input id="password" type="password" placeholder="••••••••" />
-        </Field>
-      </FieldGroup>
-    </FieldSet>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg border">
+        
+        <FieldSet>
+          <FieldGroup className="space-y-6">
+            <Field>
+              <FieldLabel htmlFor="username">Username</FieldLabel>
+              <Input id="username" type="text" placeholder="xnxx" />
+              <FieldDescription>
+                Choose a unique username for your account.
+              </FieldDescription>
+            </Field>
+
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <FieldDescription>
+                Must be at least 8 characters long.
+              </FieldDescription>
+              <Input id="password" type="password" placeholder="••••••••" />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+
+        <Button className="w-full mt-6">
+          Login
+        </Button>
+
+      </div>
+    </div>
   )
 }
