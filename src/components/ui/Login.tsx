218b92a15ce8f92d55cc36720df865ce5ad1bb0b
Keybeen <keybeen.videoeditor@gmail.com>
2026-03-01 21:52:59 +0800

    adding directories and pages for each user

--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -18,16 +18,9 @@ export function FieldInput() {
             <Field>
               <FieldLabel htmlFor="username">Username</FieldLabel>
               <Input id="username" type="text" placeholder="xnxx" />
-              <FieldDescription>
-                Choose a unique username for your account.
-              </FieldDescription>
             </Field>
-
             <Field>
               <FieldLabel htmlFor="password">Password</FieldLabel>
-              <FieldDescription>
-                Must be at least 8 characters long.
-              </FieldDescription>
               <Input id="password" type="password" placeholder="••••••••" />
             </Field>
           </FieldGroup>
